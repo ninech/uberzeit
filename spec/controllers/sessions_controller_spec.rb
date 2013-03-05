1a46a5db1476c8876890d9002c68d241b8eb2724
Raffael Schmid <raf@nine.ch>
2013-03-05 18:13:41 +0100

    fix tests for session helper

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe SessionsController do
+
+  before do
+    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:cas]
+  end
+
+  describe 'POST create' do
+    it 'creates the user when signing in' do
+      expect { get :create, provider: :cas }.to change(User, :count)
+    end
+  end
+
 end
