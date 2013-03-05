a01e06814985e3b7d09e65bffeaea1f6ded76429
Raffael Schmid <raf@nine.ch>
2013-03-05 18:19:32 +0100

    fix after login helper

--- spec/helpers/after_login_helper_spec.rb
+++ spec/helpers/after_login_helper_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
 
 describe AfterLoginHelper do
+
+  let(:user) { FactoryGirl.create(:user) }
+
   before do
-    person = Person.find_one
-    helper.sign_in(person.id)
-    @user = User.find_by_ldap_id(person.id)
+    helper.sign_in(user)
   end
 
   it 'creates a timesheet for the current user when non-existent' do
-    @user.sheets.should_not be_empty
+    user.sheets.should_not be_empty
   end
 
   it 'creates an employment for the current user when non-existent' do
-    @user.employments.should_not be_empty
+    user.employments.should_not be_empty
   end
 end
