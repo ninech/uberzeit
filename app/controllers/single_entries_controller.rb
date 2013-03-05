3f36b825fa842c1bfec5a1d479d2e2d16524c8be
Raffael Schmid <raf@nine.ch>
2013-03-05 17:26:56 +0100

    authentication for single entries controller

--- app/controllers/single_entries_controller.rb
+++ app/controllers/single_entries_controller.rb
@@ -3,48 +3,34 @@ class SingleEntriesController < ApplicationController
   load_and_authorize_resource :time_sheet
   load_and_authorize_resource :single_entry, through: :time_sheet
 
-  before_filter :load_sheet
-
   def new
-    @entry = SingleEntry.new
   end
 
   def edit
-    @entry = SingleEntry.find(params[:id])
   end
 
   def create
-    @entry = SingleEntry.new(params[:single_entry])
-    @sheet.single_entries << @entry
-    if @entry.save
-      redirect_to @sheet, :notice => 'Entry was successfully created.'
+    if @single_entry.save
+      redirect_to @time_sheet, :notice => 'Entry was successfully created.'
     else
       render :action => 'new'
     end
   end
 
   def update
-    @entry = SingleEntry.find(params[:id])
-    if @entry.update_attributes(params[:single_entry])
-      redirect_to @sheet, :notice => 'Entry was successfully updated.'
+    if @single_entry.update_attributes(params[:single_entry])
+      redirect_to @time_sheet, :notice => 'Entry was successfully updated.'
     else
       render :action => 'edit'
     end
   end
 
   def destroy
-    @entry = SingleEntry.find(params[:id])
-    if @entry.destroy
-      redirect_to @sheet, :notice => 'Entry was successfully deleted.'
+    if @single_entry.destroy
+      redirect_to @time_sheet, :notice => 'Entry was successfully deleted.'
     else
       render :action => 'edit'
     end
-  end
-
-  private
-
-  def load_sheet
-    @sheet = TimeSheet.find(params[:time_sheet_id]) unless params[:time_sheet_id].blank?
   end
 
 end
