e137d12fcecd7a4c8d5ef72e4f05f769b1428d9f
Raffael Schmid <raf@nine.ch>
2013-03-05 18:17:43 +0100

    fix user controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,57 +3,48 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+  let(:user) { FactoryGirl.create(:user) }
+
   context 'for non-signed in users' do
     it 'denies access' do
-      user = FactoryGirl.create(:user)
-      get :edit, id: user
-      response.should redirect_to(root_path)
-      flash[:error].should_not be_nil
+      expect { get :edit, id: user }.to raise_error(CanCan::AccessDenied)
     end
   end
 
   context 'for signed-in users' do
     before do
-      test_sign_in
+      test_sign_in user
     end
 
     describe 'GET "edit"' do
-      before do
-        @user = FactoryGirl.create(:user)
-      end
-
       it 'assigns the to-be edited user to @user' do
-        get :edit, id: @user
-        assigns(:user).should eq(@user)
+        get :edit, id: user.id
+        assigns(:user).should eq(user)
       end
 
       it 'renders the :edit template' do
-        get :edit, id: @user
+        get :edit, id: user
         response.should render_template :edit
       end
     end
 
     describe 'PUT "update"' do
-      before do
-        @user = FactoryGirl.create(:user, time_zone: 'Bern')
-      end
-
       context 'with valid attributes' do
         it 'changes the time zone' do
-          put :update, id: @user, user: FactoryGirl.attributes_for(:user, time_zone: 'Tokyo')
-          @user.reload
-          @user.time_zone.should eq('Tokyo')
+          put :update, id: user.id, user: FactoryGirl.attributes_for(:user, time_zone: 'Tokyo')
+          user.reload
+          user.time_zone.should eq('Tokyo')
         end
 
         it 'should redirect to updated user' do
-          put :update, id: @user, user: FactoryGirl.attributes_for(:user, time_zone: 'Tokyo')
-          response.should redirect_to edit_user_path(@user)
+          put :update, id: user.id, user: FactoryGirl.attributes_for(:user, time_zone: 'Tokyo')
+          response.should redirect_to edit_user_path(user)
         end
       end
 
       context 'with invalid attributes' do
         it 're-renders the :edit template' do
-          put :update, id: @user, user: FactoryGirl.attributes_for(:user, time_zone: 'Utopia')
+          put :update, id: user.id, user: FactoryGirl.attributes_for(:user, time_zone: 'Utopia')
           response.should render_template :edit
         end
       end
