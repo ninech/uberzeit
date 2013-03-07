1985736f795d615683be853ea650165b7b94a5ef
Raffael Schmid <raf@nine.ch>
2013-03-07 11:11:29 +0100

    move vacation calculation logic into an interactor

---
+++ app/interactors/vacation.rb
@@ -0,0 +1,45 @@
+class Vacation
+
+  def initialize(user, year)
+    @user = user
+    @year = year
+    @total = 0.0
+  end
+
+  def total_redeemable_for_this_year
+    total = employments_for_year.inject(0.0) do |sum, employment|
+      employment_duration = if employment.open_ended?
+                              year_as_range.intersect((employment.start_date..(first_day_of_year + 1.year))).duration
+                            else
+                              year_as_range.intersect((employment.start_date..employment.end_date)).duration
+                            end
+      sum + employment.workload * 0.01 * employment_duration/year_as_range.duration * default_vacation_per_year
+    end
+
+    round_to_half_work_days(total)
+  end
+
+  private
+  def first_day_of_year
+    @first_day_of_year ||= Time.zone.now.beginning_of_year.to_date
+  end
+
+  def year_as_range
+    @year_as_range ||= first_day_of_year...(first_day_of_year + 1.year)
+  end
+
+  def employments_for_year
+    @employments_for_year ||= @user.employments.between(year_as_range.min, year_as_range.max)
+  end
+
+  def default_vacation_per_year
+    @default_vacation_per_year ||= UberZeit::Config[:vacation_per_year] / 1.day * UberZeit::Config[:work_per_day]
+  end
+
+  def round_to_half_work_days(seconds)
+    half_day = UberZeit::Config[:work_per_day]*0.5
+    (seconds/half_day).round * half_day
+  end
+
+end
+
---
+++ spec/interactors/vacation_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+
+describe Vacation do
+
+  describe '#total_for_this_year' do
+
+    let(:user) { FactoryGirl.create(:user, with_sheet: false) }
+    let(:year) { 2013 }
+    let(:vacation) { Vacation.new(user, year) }
+
+    it 'returns the total available vacations for the given year' do
+      vacation.total_redeemable_for_this_year.should eq(25.work_days)
+    end
+
+    context 'rounding' do
+      it 'handles 55% correctly' do
+        employment = user.employments.first
+        employment.workload = 55
+        employment.save!
+        vacation.total_redeemable_for_this_year.should eq(14.work_days)
+      end
+
+      it 'handles 53% correctly' do
+        employment = user.employments.first
+        employment.workload = 53
+        employment.save!
+        vacation.total_redeemable_for_this_year.should eq(13.5.work_days)
+      end
+    end
+
+  end
+
+end
