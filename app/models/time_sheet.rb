b9154e4a7a64fa3f8226aa7d2eb20804a4cc0f36
Raffael Schmid <raf@nine.ch>
2013-03-12 11:00:09 +0100

    use the new Overtime-Calculator

--- app/models/time_sheet.rb
+++ app/models/time_sheet.rb
@@ -21,29 +21,7 @@ class TimeSheet < ActiveRecord::Base
   end
 
   def overtime(date_or_range)
-    if date_or_range.kind_of?(Date)
-      date = date_or_range
-      workload = user.workload_on(date)
-
-      if workload >= 100
-        # For full time position, the overtime per day is based on the excess of time relative to the planned work per day
-        remaining_work_on_date = user.planned_work(date) - total(date, :vacation)
-      else
-        # special case for people with no fulltime position
-        # calculate the daily overtime not based on the daily work hour (because it might be like 6.8 hours for 80% workload)
-        # but calculate the overtime based on the status of the current week
-        week_until_today = date.monday...date.to_date
-        whole_week = date.monday...date.next_week
-
-        effective_planned_work = user.planned_work(whole_week) - total(whole_week, :vacation)
-        remaining_work_on_date = [effective_planned_work - total(week_until_today, :work), 0].max
-      end
-
-      overtime = [total(date, :work) - remaining_work_on_date, 0].max
-    else
-      effective_planned_work = user.planned_work(date_or_range) - total(date_or_range, :vacation)
-      overtime = total(date_or_range, :work) - effective_planned_work
-    end
+    Overtime.new(user, date_or_range).total
   end
 
   def vacation(year)
