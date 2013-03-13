655f2aa0e364978d5bac0bc1502f1d431fd0da36
Steven Schmid <stsch@nine.ch>
2013-03-13 18:01:08 +0100

    fix time zone issue

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -35,7 +35,7 @@ class TimeEntry < ActiveRecord::Base
   end
 
   def range_for_other_start_time(other_start_time)
-    other_start_time = other_start_time.to_time
+    other_start_time = other_start_time
     (other_start_time..other_start_time+duration)
   end
 end
