a10ff937b59c47a0ae4c50ed5f44bce0d06db920
Steven Schmid <stsch@nine.ch>
2013-03-14 13:35:43 +0100

    refactoring of recurring schedules

--- spec/factories/recurring_schedules.rb
+++ spec/factories/recurring_schedules.rb
@@ -2,15 +2,17 @@
 
 FactoryGirl.define do
   factory :recurring_schedule do
-    ends "never"
+    enterable { FactoryGirl.build(:date_entry) }
+    ends 'never'
     ends_counter 1
-    ends_date "2013-03-12"
-    repeat_interval_type "daily"
+    ends_date Date.today
+    repeat_interval_type 'daily'
+
     daily_repeat_interval 1
     weekly_repeat_interval 1
-    weekly_repeat_weekday "MyString"
     monthly_repeat_interval 1
-    monthly_repeat_by "MyString"
     yearly_repeat_interval 1
+
+    monthly_repeat_by 'day_of_month'
   end
 end
