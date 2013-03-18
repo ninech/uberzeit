e4a1849651c81f653fb905c52e4667ecd97d038f
Steven Schmid <stsch@nine.ch>
2013-03-18 16:55:00 +0100

    fix time chunk specs

--- spec/models/time_chunk_spec.rb
+++ spec/models/time_chunk_spec.rb
@@ -4,13 +4,18 @@ describe TimeChunk do
 
   describe '#whole_day?' do
     it 'returns true if the entry is a whole day' do
-      TimeChunk.new(first_half_day: true, second_half_day: true).whole_day?.should be_true
+      entry = FactoryGirl.create(:date_entry, start_date: '2013-01-03', end_date: '2013-01-04', first_half_day: true, second_half_day: true)
+      TimeChunk.new(parent: entry).whole_day?.should be_true
     end
 
-    it 'returns false if the entru is not a whole day' do
-      TimeChunk.new(first_half_day: true, second_half_day: false).whole_day?.should be_false
-      TimeChunk.new(first_half_day: false, second_half_day: true).whole_day?.should be_false
-      TimeChunk.new(first_half_day: false, second_half_day: false).whole_day?.should be_false
+    it 'returns false if the date_entry is not a whole day' do
+      entry_first_half_day = FactoryGirl.create(:date_entry, start_date: '2013-01-03', end_date: '2013-01-04', first_half_day: true, second_half_day: false)
+      entry_second_half_day = FactoryGirl.create(:date_entry, start_date: '2013-01-03', end_date: '2013-01-04', first_half_day: false, second_half_day: true)
+      entry_no_flags = FactoryGirl.create(:date_entry, start_date: '2013-01-03', end_date: '2013-01-04', first_half_day: false, second_half_day: false)
+
+      TimeChunk.new(parent: entry_first_half_day).whole_day?.should be_false
+      TimeChunk.new(parent: entry_second_half_day).whole_day?.should be_false
+      TimeChunk.new(parent: entry_no_flags).whole_day?.should be_true
     end
   end
 
