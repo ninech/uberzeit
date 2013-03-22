4df6289db11052060ddd1009a8e3bdc2bc51fbda
Steven Schmid <stsch@nine.ch>
2013-03-22 16:42:29 +0100

    fix duration display of times longer than 24 hours

--- app/helpers/time_sheets_helper.rb
+++ app/helpers/time_sheets_helper.rb
@@ -1,19 +1,26 @@
 module TimeSheetsHelper
   def worktime_for_day(day)
-    worktime = @time_sheet.total(day, TimeType.work)
+    worktime = @time_sheet.work(day)
     if @timer && Date.parse(@timer.start_date) == day
       worktime += @timer.duration
     end
 
-    l Time.at(worktime)
+    format_duration(worktime)
   end
 
   def worktime_for_week(week)
-    worktime = @time_sheet.total(@week, TimeType.work)
+    worktime = @time_sheet.work(@week)
     if @timer && @week.include?(@timer.start_date.to_date)
       worktime += @timer.duration
     end
 
-    l Time.at(worktime)
+    format_duration(worktime)
   end
+
+  def format_duration(duration)
+    hours = duration.to_hours.to_i
+    minutes = (duration - hours * 1.hour).to_minutes.to_i
+    "%02i:%02i" % [hours, minutes]
+  end
+
 end
