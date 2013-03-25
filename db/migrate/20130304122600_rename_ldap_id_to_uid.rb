19077a718b9749b23a9d8cdb1c130631aa75211a
Steven Schmid <stsch@nine.ch>
2013-03-25 15:17:06 +0100

    rename team column ldap_id to uid

--- db/migrate/20130304122600_rename_ldap_id_to_uid.rb
+++ db/migrate/20130304122600_rename_ldap_id_to_uid.rb
@@ -1,9 +1,11 @@
 class RenameLdapIdToUid < ActiveRecord::Migration
   def up
+    rename_column :teams, :ldap_id, :uid
     rename_column :users, :ldap_id, :uid
   end
 
   def down
-    rename_column :users, :uid, :ldap_id
+    rename_column :teams, :uid, :ldap_id
+    rename_column :users, :ldap_id, :uid
   end
 end
