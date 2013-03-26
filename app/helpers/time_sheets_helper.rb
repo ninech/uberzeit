438922744f81b7190d3b48dfa87a8aadd6e40455
Steven Schmid <stsch@nine.ch>
2013-03-26 15:15:48 +0100

    make non-standard calculation percentages visible on the time sheet

--- app/helpers/time_sheets_helper.rb
+++ app/helpers/time_sheets_helper.rb
@@ -23,4 +23,12 @@ module TimeSheetsHelper
     "%02i:%02i" % [hours, minutes]
   end
 
+  def standard_calculation?(time_type)
+    time_type.calculation_factor == 1.0
+  end
+
+  def time_type_calculation_percentage(time_type)
+    calculation_percent = time_type.calculation_factor * 100.0
+    "(%s%%)" % number_with_precision(calculation_percent, precision: 2, strip_insignificant_zeros: true)
+  end
 end
