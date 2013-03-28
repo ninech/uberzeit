7d0c5e0a8b5f8e89cb8305033b70437e87a4c88a
Steven Schmid <stsch@nine.ch>
2013-03-28 11:23:17 +0100

    cleanup time sheets helper

--- app/helpers/time_sheets_helper.rb
+++ app/helpers/time_sheets_helper.rb
@@ -1,19 +1,11 @@
 module TimeSheetsHelper
   def worktime_for_day(day)
-    worktime = @time_sheet.work(day)
-    if @timer && Date.parse(@timer.start_date) == day
-      worktime += @timer.duration
-    end
-
+    worktime = work_for_range(day)
     format_duration(worktime)
   end
 
   def worktime_for_week(week)
-    worktime = @time_sheet.work(@week)
-    if @timer && @week.include?(@timer.start_date.to_date)
-      worktime += @timer.duration
-    end
-
+    worktime = work_for_range(week)
     format_duration(worktime)
   end
 
@@ -44,5 +36,20 @@ module TimeSheetsHelper
   def time_type_calculation_percentage(time_type)
     calculation_percent = time_type.calculation_factor * 100.0
     "%s%%" % number_with_precision(calculation_percent, precision: 2, strip_insignificant_zeros: true)
+  end
+
+  private
+
+  def work_for_range(date_or_range)
+    @time_sheet.work(date_or_range) + timer_duration(date_or_range)
+  end
+
+  def timer_duration(date_or_range)
+    range = date_or_range.to_range.to_date_range
+    if @timer && range.include?(@timer.start_date.to_date)
+      @timer.duration
+    else
+      0
+    end
   end
 end
