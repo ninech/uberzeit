c54cdc9e18dc0848d1f0db5fa0839bc9a2300110
Raffael Schmid <raf@nine.ch>
2013-04-04 15:49:13 +0200

    order the timeers by their start time

--- app/controllers/time_sheets_controller.rb
+++ app/controllers/time_sheets_controller.rb
@@ -22,7 +22,7 @@ class TimeSheetsController < ApplicationController
     unless @timer.nil?
       @timer_active = (@timer.start_date.to_date == @day.to_date) # (params[:date] || Time.current.to_date.to_s(:db))
     end
-    @timers_other_days = @time_sheet.timers.others(@day)
+    @timers_other_days = @time_sheet.timers.others(@day).order('start_time')
 
     @public_holiday = PublicHoliday.on(@day).first
 
