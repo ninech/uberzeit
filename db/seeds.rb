59e5c9df78c2a1839a0d846adbff61b183a55ea5
Nils Caspar <nils@nine.ch>
2013-04-12 15:08:33 +0200

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 if TimeType.count == 0
-  TimeType.create(name: 'Work', is_work: true, is_vacation: false, absence: false)
-  TimeType.create(name: 'On Duty', is_work: true, is_vacation: false, calculation_factor: 1.1, absence: false)
-  TimeType.create(name: 'Vacation', is_work: false, is_vacation: true, absence: true)
-  TimeType.create(name: 'Illness', is_work: false, is_vacation: false, absence: true)
-  TimeType.create(name: 'Compensation', is_work: false, is_vacation: false, calculation_factor: 0.0, absence: true)
-  TimeType.create(name: 'Absence by law', is_work: false, is_vacation: false, absence: true)
+  TimeType.create(name: 'Work', is_work: true, is_vacation: false)
+  TimeType.create(name: 'On Duty', is_work: true, is_vacation: false, calculation_factor: 1.1)
+  TimeType.create(name: 'Vacation', is_work: false, is_vacation: true)
+  TimeType.create(name: 'Illness', is_work: false, is_vacation: false)
+  TimeType.create(name: 'Compensation', is_work: false, is_vacation: false, calculation_factor: 0.0)
+  TimeType.create(name: 'Absence by law', is_work: false, is_vacation: false)
 end
