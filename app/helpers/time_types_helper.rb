0f1b998ddf6c953722102df7e78e3dfbc0b9ab2e
Steven Schmid <stsch@nine.ch>
2013-04-17 17:05:07 +0200

    add icon

--- app/helpers/time_types_helper.rb
+++ app/helpers/time_types_helper.rb
@@ -3,7 +3,7 @@ module TimeTypesHelper
   def list_available_icons(active = nil)
     icons_available = [ 'exchange','bell-alt','beer','coffee','food','user-md','ambulance',
                         'medkit','github-alt','book','briefcase','bullhorn','camera','coffee',
-                        'cog','comments','food','circle-blank','circle','group','home','legal',
+                        'cog','comments','fighter-jet','circle-blank','circle','group','home','legal',
                         'road','truck','wrench','fire','ban-circle','shopping-cart','plane',
                         'globe','leaf','adjust','asterisk']
 
