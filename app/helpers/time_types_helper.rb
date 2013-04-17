520746d023b604724a2458d4edf294f8eab89704
Steven Schmid <stsch@nine.ch>
2013-04-17 14:27:52 +0200

    add icon picker for time types

--- app/helpers/time_types_helper.rb
+++ app/helpers/time_types_helper.rb
@@ -1,2 +1,19 @@
 module TimeTypesHelper
+
+  def list_available_icons(active = nil)
+    icons_available = [ 'exchange','bell-alt','beer','coffee','food','user-md','ambulance',
+                        'medkit','github-alt','book','briefcase','bullhorn','camera','coffee',
+                        'cog','comments','food','circle-blank','circle','group','home','legal',
+                        'road','truck','wrench']
+
+    content_tag(:ul, class: 'list-of-icons' ) do
+      icons_available.collect do |icon_name|
+        content_tag(:li) do
+          css_classes = ["icon icon-#{icon_name}"]
+          css_classes << ["active"] if icon_name == active
+          content_tag(:i, '', :'data-icon' => icon_name, class: css_classes.join(' '))
+        end
+      end.join.html_safe
+    end
+  end
 end
