69c8c26bab469e2cb854a9beb967040b238e0919
Steven Schmid <stsch@nine.ch>
2013-04-17 12:47:56 +0200

    time types: remove unnecessary i18n keys and improve display of flash messages

--- app/controllers/time_types_controller.rb
+++ app/controllers/time_types_controller.rb
@@ -13,7 +13,7 @@ class TimeTypesController < ApplicationController
 
   def create
     if @time_type.save
-      redirect_to time_types_path, :notice => t('model_successfully_created', model: TimeType.model_name.human)
+      redirect_to time_types_path, flash: {success: t('model_successfully_created', model: TimeType.model_name.human)}
     else
       render :action => 'new'
     end
@@ -21,7 +21,7 @@ class TimeTypesController < ApplicationController
 
   def update
     if @time_type.update_attributes(params[:time_type])
-      redirect_to time_types_path, :notice => t('model_successfully_updated', model: TimeType.model_name.human)
+      redirect_to time_types_path, flash: {success: t('model_successfully_updated', model: TimeType.model_name.human)}
     else
       render :action => 'edit'
     end
@@ -29,7 +29,7 @@ class TimeTypesController < ApplicationController
 
   def destroy
     if @time_type.destroy
-      redirect_to time_types_path, :notice => t('model_successfully_deleted', model: TimeType.model_name.human)
+      redirect_to time_types_path, flash: {success: t('model_successfully_deleted', model: TimeType.model_name.human)}
     else
       render :action => 'edit'
     end
