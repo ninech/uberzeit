537d8f6ed0a47d56866772b18e8687e6825ef8d1
Steven Schmid <stsch@nine.ch>
2013-04-17 13:14:30 +0200

    public holidays: remove unnecessary i18n keys and improve display of flash messages

--- app/controllers/public_holidays_controller.rb
+++ app/controllers/public_holidays_controller.rb
@@ -22,7 +22,7 @@ class PublicHolidaysController < ApplicationController
     @public_holiday = PublicHoliday.new(params[:public_holiday])
     @public_holiday.end_date = @public_holiday.start_date
     if @public_holiday.save
-      redirect_to public_holidays_path, :notice => t('model_successfully_created', model: PublicHoliday.model_name.human)
+      redirect_to public_holidays_path, flash: {success: t('model_successfully_created', model: PublicHoliday.model_name.human)}
     else
       render :action => 'new'
     end
@@ -32,7 +32,7 @@ class PublicHolidaysController < ApplicationController
     @public_holiday = PublicHoliday.find(params[:id])
     params[:public_holiday][:end_date] = params[:public_holiday][:start_date]
     if @public_holiday.update_attributes(params[:public_holiday])
-      redirect_to public_holidays_path, :notice => t('model_successfully_updated', model: PublicHoliday.model_name.human)
+      redirect_to public_holidays_path, flash: {success: t('model_successfully_updated', model: PublicHoliday.model_name.human)}
     else
       render :action => 'edit'
     end
@@ -41,7 +41,7 @@ class PublicHolidaysController < ApplicationController
   def destroy
     @public_holiday = PublicHoliday.find(params[:id])
     if @public_holiday.destroy
-      redirect_to public_holidays_path, :notice => t('model_successfully_deleted', model: PublicHoliday.model_name.human)
+      redirect_to public_holidays_path, flash: {success: t('model_successfully_deleted', model: PublicHoliday.model_name.human)}
     else
       render :action => 'edit'
     end
