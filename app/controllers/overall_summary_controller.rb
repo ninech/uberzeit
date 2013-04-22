01fed66ea733ce811335a0bd0021aa6034503007
Steven Schmid <stsch@nine.ch>
2013-04-22 17:50:52 +0200

    create monthly vacation summary

--- app/controllers/overall_summary_controller.rb
+++ app/controllers/overall_summary_controller.rb
@@ -1,6 +1,8 @@
 class OverallSummaryController < ApplicationController
   def vacation
+
     summarize_overall = SummarizeOverall.new(Date.current.year, Date.current.month)
     @rows = summarize_overall.vacation
+    @month_date = Date.new(Date.current.year, Date.current.month)
   end
 end
--- app/interactors/summarize_overall.rb
+++ app/interactors/summarize_overall.rb
@@ -1,20 +1,35 @@
 class SummarizeOverall
-  def initialize(year_or_range, team = nil)
-
+  def initialize(year, month = nil, team = nil)
+    @team = team
+    @year = year
+    @month = month
   end
 
   def vacation
+    users.collect do |user|
+      total_redeemable = CalculateTotalRedeemableVacation.new(user, @year).total_redeemable_for_year
+      redeemed_before = (1..@month-1).inject(0) { |sum, month_before| sum + SummarizeOverall.redeemed_in_month(user, @year, month_before) }
+      redeemed_this_month = SummarizeOverall.redeemed_in_month(user, @year, @month)
 
+      remaining = total_redeemable - (redeemed_before + redeemed_this_month)
 
+      { user: user, total_redeemable: total_redeemable, redeemed_before: redeemed_before, redeemed_this_month: redeemed_this_month, remaining: remaining }
+    end
   end
 
   private
 
-  def employees
+  def users
     if @team.nil?
-      @team.leaders_and_members
-    else
       User.all
+    else
+      @team.leaders_and_members
     end
   end
+
+  def self.redeemed_in_month(user, year, month)
+    range = UberZeit.month_as_range(year, month)
+    CalculateRedeemedVacation.new(user, range).total
+  end
+
 end
