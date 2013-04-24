6f5bda850b4d9e961e0179e33cad9ee7513a2e48
Steven Schmid <stsch@nine.ch>
2013-04-24 15:35:37 +0200

    remove unnecessary controller method

--- app/controllers/summaries/absences/my_absences_controller.rb
+++ app/controllers/summaries/absences/my_absences_controller.rb
@@ -8,12 +8,4 @@ class Summaries::Absences::MyAbsencesController < ApplicationController
     @table = Summarize::TableWithInterval.new(Summarize::Summarizer::Absences, [@user], @range, 1.month)
     @entries = @table.entries
   end
-
-  def month
-    @year = params[:year].to_i
-    @month = params[:month].to_i
-    @range = UberZeit.month_as_range(@year, @month)
-    @table = Summarize::TableWithInterval.new(Summarize::Summarizer::Absences, [@user], @range, 1.week, @range.min.monday)
-    @entries = @table.entries
-  end
 end
