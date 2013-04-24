f2cc81cd431360ab3517a5f9bece4cc98099034a
Steven Schmid <stsch@nine.ch>
2013-04-24 17:45:17 +0200

    add vacation controller

---
+++ app/controllers/summaries/vacation/vacation_controller.rb
@@ -0,0 +1,29 @@
+class Summaries::Vacation::VacationController < ApplicationController
+
+  before_filter :load_team
+  before_filter :load_teams
+
+  def year
+    @teams = Team.all
+    @year = params[:year].to_i
+    @table = Summarize::Table.new(Summarize::Summarizer::Vacation, @team || User.all, UberZeit.year_as_range(@year))
+    @entries = @table.entries
+  end
+
+  def month
+    @year = params[:year].to_i
+    @month = params[:month].to_i
+    @table = Summarize::Table.new(Summarize::Summarizer::Vacation, @team || User.all, UberZeit.month_as_range(@year, @month))
+    @entries = @table.entries
+  end
+
+  private
+
+  def load_team
+    @team = Team.find(params[:team_id]) unless params[:team_id].blank?
+  end
+
+  def load_teams
+    @teams = Team.all
+  end
+end
