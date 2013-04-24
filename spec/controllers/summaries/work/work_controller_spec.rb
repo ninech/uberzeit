cb1c5a445308baba3618e942c92d13f84c8983a2
Steven Schmid <stsch@nine.ch>
2013-04-24 11:43:52 +0200

    remove unnecessary user param from work controller

--- spec/controllers/summaries/work/work_controller_spec.rb
+++ spec/controllers/summaries/work/work_controller_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe Summaries::Work::WorkController do
   render_views
 
-  let(:user) { FactoryGirl.create(:user, with_sheet: true) }
+  let(:user) { FactoryGirl.create(:user) }
   let(:year) { 2013 }
   let(:month) { 3 }
 
   context 'for non-signed in users' do
     it 'denies access' do
-      expect { get :year, user_id: user, year: year }.to raise_error(CanCan::AccessDenied)
+      expect { get :year, year: year }.to raise_error(CanCan::AccessDenied)
     end
   end
 
@@ -21,27 +21,27 @@ describe Summaries::Work::WorkController do
 
     describe 'GET "year"' do
       it 'assigns the correct instance variables' do
-        get :year, user_id: user, year: year
+        get :year, year: year
         assigns(:year).should_not be_nil
         assigns(:entries).should_not be_nil
       end
 
       it 'renders the :year template' do
-        get :year, user_id: user, year: year
+        get :year, year: year
         response.should render_template :year
       end
     end
 
     describe 'GET "month"' do
       it 'assigns the correct instance variables' do
-        get :month, user_id: user, year: year, month: month
+        get :month, year: year, month: month
         assigns(:year).should_not be_nil
         assigns(:month).should_not be_nil
         assigns(:entries).should_not be_nil
       end
 
       it 'renders the :month template' do
-        get :month, user_id: user, year: year, month: month
+        get :month, year: year, month: month
         response.should render_template :month
       end
     end
