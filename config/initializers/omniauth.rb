92e5c3459480fa21162dea91aa5829585a7fc66f
Steven Schmid <stsch@nine.ch>
2013-05-07 14:27:47 +0200

    fix sso url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,11 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development?
     provider :developer, :fields => [:email], :uid_field => :email
   end
-  provider :cas, url: 'https://sso-staging.nine.ch/login', disable_ssl_verification: false
+  if Rails.env.production?
+    provider :cas, url: 'https://sso.nine.ch/login', disable_ssl_verification: false
+  else
+    provider :cas, url: "https://sso-#{Rails.env}.nine.ch/login", disable_ssl_verification: false
+  end
 end
 
 OmniAuth.config.logger = Rails.logger
