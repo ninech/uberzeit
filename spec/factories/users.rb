537ae8965c5030dc2a26606616e12d4b81de5d20
Steven Schmid <stsch@nine.ch>
2013-05-08 12:16:25 +0200

    add more validations to employments refs #6046

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,7 +8,7 @@ FactoryGirl.define do
       with_sheet true
     end
 
-    after(:create) { |user, evaluator| user.employments << FactoryGirl.create(:employment, user: user) if evaluator.with_employment && user.employments.empty? }
+    after(:create) { |user, evaluator| user.employments << FactoryGirl.create(:employment, user: user, start_date: '2013-01-01', end_date: '2013-12-31') if evaluator.with_employment && user.employments.empty? }
     after(:create) { |user, evaluator| user.time_sheets << FactoryGirl.create(:time_sheet, user: user) if evaluator.with_sheet && user.time_sheets.empty? }
 
     factory :leader do
--- spec/models/employment_spec.rb
+++ spec/models/employment_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Employment do
+  let(:user) { FactoryGirl.create(:user, with_employment: false) }
+
   it 'has a valid factory' do
     FactoryGirl.create(:employment).should be_valid
   end
@@ -8,6 +10,22 @@ describe Employment do
   it 'cannot be deleted if it is the last one for the user' do
     employment = FactoryGirl.create(:employment)
     employment.destroy.should be_false
+  end
+
+  it 'cannot be saved if it overlaps with another entry' do
+    existing_entry = FactoryGirl.create(:employment, start_date: '2013-05-06', end_date: '2013-05-08', user: user)
+    user.reload
+
+    new_entry = FactoryGirl.build(:employment, start_date: '2013-05-07', end_date: '2013-05-10', user: user)
+    new_entry.save.should be_false
+  end
+
+  it 'cannot be saved if its an open-ended entry and there is already an existing open-ended entry' do
+    existing_entry = FactoryGirl.create(:employment, start_date: '2013-05-06', end_date: nil, user: user)
+    user.reload
+
+    new_entry = FactoryGirl.build(:employment, start_date: '2013-05-07', end_date: nil, user: user)
+    new_entry.save.should be_false
   end
 
   it 'makes sure the workload is between 1 and 100' do
@@ -21,10 +39,11 @@ describe Employment do
     before do
       # create tricky employment scheme
       @user = FactoryGirl.create(:user, with_employment: false)
-      @user.employments << FactoryGirl.create(:employment, start_date: '2012-06-06', end_date: '2012-12-31', workload: 100)
-      @user.employments << FactoryGirl.create(:employment, start_date: '2013-01-01', end_date: '2013-02-10', workload: 50)
-      @user.employments << FactoryGirl.create(:employment, start_date: '2013-02-11', end_date: '2013-02-11', workload: 80)
-      @user.employments << FactoryGirl.create(:employment, start_date: '2013-02-12', end_date: nil, workload: 40)
+      FactoryGirl.create(:employment, start_date: '2012-06-06', end_date: '2012-12-31', workload: 100, user: @user)
+      FactoryGirl.create(:employment, start_date: '2013-01-01', end_date: '2013-02-10', workload: 50, user: @user)
+      FactoryGirl.create(:employment, start_date: '2013-02-11', end_date: '2013-02-11', workload: 80, user: @user)
+      FactoryGirl.create(:employment, start_date: '2013-02-12', end_date: nil, workload: 40, user: @user)
+      @user.reload
     end
 
     it 'can be deleted if there are more than one employments for the user' do
@@ -42,12 +61,6 @@ describe Employment do
     it 'returns the employment for a specific date' do
       @user.employments.on('2013-02-11'.to_date).workload.should eq(80)
       @user.employments.on('2013-02-12'.to_date).workload.should eq(40)
-    end
-
-    it 'resolves conflicts between two open ended employments' do
-      open_ended = @user.employments.find { |employment| employment.open_ended? }
-      @user.employments << FactoryGirl.create(:employment, start_date: '2013-06-12', end_date: nil, workload: 20)
-      open_ended.reload.open_ended?.should be_false
     end
   end
 end
