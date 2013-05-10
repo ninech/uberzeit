5a8dd7fe77e5ff458f80732b2cfbd51804067a75
Steven Schmid <stsch@nine.ch>
2013-05-10 13:31:15 +0200

    add beautiful error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,34 @@ class ApplicationController < ActionController::Base
   before_filter :ensure_logged_in
 
   unless Rails.env.test?
-    rescue_from CanCan::AccessDenied do |error|
-      flash[:alert] = error.message
-      redirect_to root_path
-    end
+    rescue_from Exception, with: :render_500
+    rescue_from ActionController::RoutingError, with: :render_404
+    rescue_from ActionController::UnknownController, with: :render_404
+    rescue_from AbstractController::ActionNotFound, with: :render_404 # To prevent Rails 3.2.8 deprecation warnings
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    rescue_from CanCan::AccessDenied, with: :render_403
   end
 
   private
+
+  def render_exception(status = 500, exception)
+    notify_airbrake(exception) if status >= 500
+    @exception = exception
+    @status = status
+    render template: "errors/error", formats: [:html], layout: 'application', status: @status
+  end
+
+  def render_403(exception = nil)
+    render_exception(403, exception)
+  end
+
+  def render_404(exception = nil)
+    render_exception(404, exception)
+  end
+
+  def render_500(exception = nil)
+    render_exception(500, exception)
+  end
 
   def ensure_logged_in
     if current_user.nil?
