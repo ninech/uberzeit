ff6a395b7a79c22f48b2ecdd3f0fb8a9cd5a90e5
Steven Schmid <stsch@nine.ch>
2013-05-13 10:35:03 +0200

    add tests for the display of menu elements

--- spec/features/roles_and_rights_spec.rb
+++ spec/features/roles_and_rights_spec.rb
@@ -21,73 +21,92 @@ describe 'Roles and Rights' do
       login(user)
     end
 
-    describe 'time sheets' do
-      it 'has access to own time sheet' do
-        should_have_access_to time_sheet_path(user.current_time_sheet)
+    context 'access' do
+      describe 'time sheets' do
+        it 'has access to own time sheet' do
+          should_have_access_to time_sheet_path(user.current_time_sheet)
+        end
+
+        it 'has no access to another time sheet' do
+          should_not_have_access_to time_sheet_path(another_user.current_time_sheet)
+        end
       end
 
-      it 'has no access to another time sheet' do
-        should_not_have_access_to time_sheet_path(another_user.current_time_sheet)
+      describe 'absences' do
+        it 'has access to own absences' do
+          should_have_access_to time_sheet_absences_path(user.current_time_sheet)
+        end
+
+        it 'has no access to absences of other users' do
+          should_not_have_access_to time_sheet_absences_path(another_user.current_time_sheet)
+        end
+      end
+
+      describe 'summaries' do
+        it 'has access to own summaries' do
+          should_have_access_to user_summaries_work_year_path(user.current_time_sheet, year)
+          should_have_access_to user_summaries_absence_year_path(user.current_time_sheet, year)
+        end
+
+        it 'has no access to absences of other users' do
+          should_not_have_access_to user_summaries_work_year_path(another_user.current_time_sheet, year)
+          should_not_have_access_to user_summaries_absence_year_path(another_user.current_time_sheet, year)
+        end
+
+        it 'has access to overall absence summary' do
+          user && another_user
+          visit calendar_summaries_absence_users_path(year, month)
+
+          page.should have_selector('td', text: user.display_name)
+          page.should have_selector('td', text: another_user.display_name)
+        end
+
+        it 'receives an empty view for overall work summary' do
+          user && another_user
+          visit year_summaries_work_users_path(year, month)
+
+          page.should_not have_selector('td', text: user.display_name)
+          page.should_not have_selector('td', text: another_user.display_name)
+        end
+
+        it 'receives an empty view for overall vacation summary' do
+          user && another_user
+          visit year_summaries_vacation_users_path(year, month)
+
+          page.should_not have_selector('td', text: user.display_name)
+          page.should_not have_selector('td', text: another_user.display_name)
+        end
+      end
+
+      describe 'manage' do
+        it 'has no acccess to public holidays' do
+          should_not_have_access_to public_holidays_path
+        end
+
+        it 'has no acccess to users' do
+          should_not_have_access_to users_path
+        end
+
+        it 'has no access to time types' do
+          should_not_have_access_to time_types_path
+        end
       end
     end
 
-    describe 'absences' do
-      it 'has access to own absences' do
-        should_have_access_to time_sheet_absences_path(user.current_time_sheet)
-      end
+    context 'UI' do
+      it 'shows the correct menu items for this role' do
+        visit user_summaries_work_year_path(user.current_time_sheet, year)
 
-      it 'has no access to absences of other users' do
-        should_not_have_access_to time_sheet_absences_path(another_user.current_time_sheet)
-      end
-    end
+        page.should have_selector('.navigation > ul > li', text: 'Zeiterfassung')
+        page.should have_selector('.navigation > ul > li', text: 'Absenzen')
+        page.should have_selector('.navigation > ul > li', text: 'Berichte')
+        page.should_not have_selector('.navigation > ul > li', text: 'Verwalten')
 
-    describe 'summaries' do
-      it 'has access to own summaries' do
-        should_have_access_to user_summaries_work_year_path(user.current_time_sheet, year)
-        should_have_access_to user_summaries_absence_year_path(user.current_time_sheet, year)
-      end
-
-      it 'has no access to absences of other users' do
-        should_not_have_access_to user_summaries_work_year_path(another_user.current_time_sheet, year)
-        should_not_have_access_to user_summaries_absence_year_path(another_user.current_time_sheet, year)
-      end
-
-      it 'has access to overall absence summary' do
-        user && another_user
-        visit calendar_summaries_absence_users_path(year, month)
-
-        page.should have_selector('td', text: user.display_name)
-        page.should have_selector('td', text: another_user.display_name)
-      end
-
-      it 'receives an empty view for overall work summary' do
-        user && another_user
-        visit year_summaries_work_users_path(year, month)
-
-        page.should_not have_selector('td', text: user.display_name)
-        page.should_not have_selector('td', text: another_user.display_name)
-      end
-
-      it 'receives an empty view for overall vacation summary' do
-        user && another_user
-        visit year_summaries_vacation_users_path(year, month)
-
-        page.should_not have_selector('td', text: user.display_name)
-        page.should_not have_selector('td', text: another_user.display_name)
-      end
-    end
-
-    describe 'manage' do
-      it 'has no acccess to public holidays' do
-        should_not_have_access_to public_holidays_path
-      end
-
-      it 'has no acccess to users' do
-        should_not_have_access_to users_path
-      end
-
-      it 'has no access to time types' do
-        should_not_have_access_to time_types_path
+        page.should have_selector('.sub-nav > dd', text: 'Meine Arbeitszeit')
+        page.should have_selector('.sub-nav > dd', text: 'Meine Absenzen')
+        page.should have_selector('.sub-nav > dd', text: 'Absenzen Mitarbeiter')
+        page.should_not have_selector('.sub-nav > dd', text: 'Arbeitszeit Mitarbeiter')
+        page.should_not have_selector('.sub-nav > dd', text: 'Ferien Mitarbeiter')
       end
     end
   end
