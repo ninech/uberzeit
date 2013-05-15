4e650b5081ea17208f77194b8a552c5c46c7ea7f
Steven Schmid <stsch@nine.ch>
2013-05-15 15:00:38 +0200

    add todo comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   if Rails.env.staging? || Rails.env.production?
     rescue_from Exception, with: :render_500
-    rescue_from ActionController::RoutingError, with: :render_404
+    rescue_from ActionController::RoutingError, with: :render_404 # TODO: DOES NOT WITH RAILS 3.2 ANYMORE! Workarounds are ugly...
     rescue_from ActionController::UnknownController, with: :render_404
     rescue_from AbstractController::ActionNotFound, with: :render_404 # To prevent Rails 3.2.8 deprecation warnings
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
