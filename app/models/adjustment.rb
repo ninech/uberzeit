4110d067660b9236c57e87ab98715694b9c82005
Steven Schmid <stsch@nine.ch>
2013-05-28 12:19:53 +0200

    add form

--- app/models/adjustment.rb
+++ app/models/adjustment.rb
@@ -4,17 +4,21 @@ class Adjustment < ActiveRecord::Base
   belongs_to :time_sheet
   belongs_to :time_type
 
-  attr_accessible :date, :duration, :label, :time_sheet_id, :time_type_id, :user
+  attr_accessible :date, :duration, :label, :time_sheet_id, :time_type_id, :user_id
 
   validates_presence_of       :time_sheet, :time_type, :date, :duration
   validates_numericality_of   :duration
   validates_date              :date
 
-  def user=(user)
-    self.time_sheet = user.current_time_sheet
+  def user_id=(user_id)
+    self.time_sheet = User.find(user_id).current_time_sheet
+  end
+
+  def user_id
+    time_sheet && time_sheet.user && time_sheet.user.id
   end
 
   def user
-    self.time_sheet.user
+    time_sheet && time_sheet.user
   end
 end
