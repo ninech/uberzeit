3a3f3bbeb8ea7c4dbcfe45a5a17eae2386c2a552
Steven Schmid <stsch@nine.ch>
2013-05-28 17:43:48 +0200

    add tests for summarizer & adjustments

--- app/interactors/summarize/summarizer/absences.rb
+++ app/interactors/summarize/summarizer/absences.rb
@@ -16,24 +16,24 @@ class Summarize::Summarizer::Absences
   end
 
   def summarize
-    Hash[TimeType.absence.collect { |time_type| [time_type, sum_of_time_type(time_type)] }]
+    Hash[TimeType.absence.collect { |absence| [absence, sum_of_absence(absence)] }]
   end
 
-  def sum_of_time_type(time_type)
+  def sum_of_absence(absence)
     return 0 if @time_sheet.nil?
 
-    chunks = @time_sheet.find_chunks(@range, time_type)
+    chunks = @time_sheet.find_chunks(@range, absence)
     chunks.ignore_exclusion_flag = true # include time types with exclusion flag in calculation (e.g. compensation)
 
-    total = if time_type.is_vacation?
+    total = if absence.is_vacation?
               # vacation adjustments are added to the reedemable days
               chunks.total
             else
-              chunks.total + duration_of_adjustments(time_type)
+              chunks.total + duration_of_adjustments(absence)
             end
   end
 
-  def duration_of_adjustments(time_type)
-    @time_sheet.adjustments.where(time_type_id: time_type).in(@range).sum(&:duration)
+  def duration_of_adjustments(absence)
+    @time_sheet.adjustments.where(time_type_id: absence).in(@range).sum(&:duration)
   end
 end
