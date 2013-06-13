ea0b8a2d085a382e802ae3de37ac0a9efe08fe59
Raffael Schmid <raf@nine.ch>
2013-06-13 18:02:06 +0200

    fix multi mass assign stuff

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   belongs_to :customer, primary_key: :id
 
-  attr_accessible :name, :customer
+  attr_accessible :name, :customer, :customer_id
 
   validates_presence_of :customer, :name
 
