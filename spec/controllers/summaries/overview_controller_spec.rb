2b8b65bd5d5fa78d53563e724b81e7d2dbe95334
Steven Schmid <stsch@nine.ch>
2013-06-17 11:04:29 +0200

    make sure team absences show a team member only once

--- spec/controllers/summaries/overview_controller_spec.rb
+++ spec/controllers/summaries/overview_controller_spec.rb
@@ -33,6 +33,30 @@ describe Summaries::OverviewController do
         get :index, user_id: user
         response.should render_template :index
       end
+
+      describe 'team absences' do
+        let!(:another_team) { FactoryGirl.create(:team) }
+        let!(:another_user) { FactoryGirl.create(:user, teams: user.teams | [another_team]) }
+
+        before do
+          user.teams << another_team
+          Timecop.freeze('2013-07-20 12:00:00')
+        end
+
+        it 'shows a team member only once even when she shares the same teams with the current user' do
+          FactoryGirl.create(:absence, time_sheet: another_user.current_time_sheet, start_date: '2013-07-22', end_date: '2013-07-22')
+          get :index, user_id: user
+          assigns(:team_absences).should include('2013-07-22'.to_date)
+          assigns(:team_absences)['2013-07-22'.to_date].should have(1).absences
+        end
+
+        it 'does not show the current user in the team absences' do
+          FactoryGirl.create(:absence, time_sheet: user.current_time_sheet, start_date: '2013-07-22', end_date: '2013-07-22')
+          get :index, user_id: user
+          assigns(:team_absences).should_not include('2013-07-22'.to_date)
+        end
+      end
+
     end
 
   end
