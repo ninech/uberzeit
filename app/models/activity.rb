4dd5741ca822d4a6be2db96f3c51dcfcd6865e6c
Raffael Schmid <raf@nine.ch>
2013-06-19 09:26:04 +0200

    validate the duration

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,6 +5,7 @@ class Activity < ActiveRecord::Base
   attr_accessible :customer_id, :date, :description, :duration, :otrs_ticket_id, :project_id, :redmine_ticket_id, :activity_type_id, :user_id
 
   validates_presence_of :user, :activity_type, :date, :duration
+  validates_numericality_of :duration, greater_than: 0
 
   scope :by_user, ->(user) { where(user_id: user)}
   scope :by_redmine_ticket, ->(redmine_ticket_id) { where(redmine_ticket_id: redmine_ticket_id) }
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Activity do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    let(:activity) { Activity.new }
+
+    it 'it does not accept a duration of zero' do
+      activity.duration = 0
+      activity.valid?.should be_false
+      activity.should have(1).errors_on(:duration)
+    end
+  end
+
 end
