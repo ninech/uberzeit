57d65c99ec68caf9d1e4ece94dc857a2fdc910e9
Raffael Schmid <raf@nine.ch>
2013-06-21 11:07:38 +0200

    add tests for the validation of Project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:project) { Project.new }
+
+    it 'it does not accept a blank name' do
+      project.name = ''
+      project.should_not be_valid
+      project.should have(1).errors_on(:name)
+    end
+  end
 end
+
