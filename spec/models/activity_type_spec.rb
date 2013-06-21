8aab6809f2072a8fb9fb0c7a2a4daba8948a200c
Raffael Schmid <raf@nine.ch>
2013-06-21 11:08:58 +0200

    add validation and tests for ActivityType, refs #6648

--- spec/models/activity_type_spec.rb
+++ spec/models/activity_type_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe ActivityType do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:activity_type) { ActivityType.new }
+
+    it 'it does not accept a blank name' do
+      activity_type.name = ''
+      activity_type.should_not be_valid
+      activity_type.should have(1).errors_on(:name)
+    end
+  end
 end
