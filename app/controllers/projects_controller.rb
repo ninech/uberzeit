204c04e0fc16ac408fb8682f5c9827114a969e3a
Raffael Schmid <raf@nine.ch>
2013-06-21 09:29:33 +0200

    index view for the projects, refs #6645

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,8 @@
 class ProjectsController < ApplicationController
+  load_and_authorize_resource
+
   def index
+    @projects = @projects.includes(:customer).all.group_by { |project| project.customer }
   end
 
   def create
