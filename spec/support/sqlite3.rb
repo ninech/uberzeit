61faf314c705c5906ffc28df8deaf144cc207be6
Nils Caspar <nils@nine.ch>
2013-07-16 13:25:48 +0200

    Make it nicer

--- spec/support/sqlite3.rb
+++ spec/support/sqlite3.rb
@@ -1,5 +1,5 @@
 def in_memory_database?
-  Rails.env == "test" and
+  Rails.env.test? and
     ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLiteAdapter ||
       ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLite3Adapter and
     Rails.configuration.database_configuration['test']['database'] == ':memory:'
