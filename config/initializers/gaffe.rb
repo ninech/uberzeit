fc81e65943c1a61936ea4844e954455f9d339b23
Raffael Schmid <raf@nine.ch>
2013-08-14 10:50:11 +0200

    remove consider local switch

--- config/initializers/gaffe.rb
+++ config/initializers/gaffe.rb
@@ -2,4 +2,5 @@ Gaffe.configure do |config|
   config.errors_controller = ErrorsController
 end
 Gaffe.enable!
-Rails.application.config.consider_all_requests_local = false
+# Uncomment for development
+#Rails.application.config.consider_all_requests_local = false
