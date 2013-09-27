6f8abdaca5e6d79ca115a85865413be330a76546
Raffael Schmid <raf@nine.ch>
2013-09-27 17:24:18 +0200

    fix migration to consolidate correctomente (remufos timeentros)

--- db/migrate/20130924133509_rename_recurring_schedules_to_absence_schedules.rb
+++ db/migrate/20130924133509_rename_recurring_schedules_to_absence_schedules.rb
@@ -1,15 +1,13 @@
 class RenameRecurringSchedulesToAbsenceSchedules < ActiveRecord::Migration
   def up
     rename_table :recurring_schedules, :absence_schedules
+    AbsenceSchedule.delete_all!(enterable_type: 'TimeEntry')
     remove_column :absence_schedules, :enterable_type
     rename_column :absence_schedules, :enterable_id, :absence_id
     add_index :absence_schedules, :absence_id
   end
 
   def down
-    remove_index :absence_schedules, :absence_id
-    add_column :absence_schedules, :enterable_type, :string
-    rename_column :absence_schedules, :absence_id, :enterable_id
-    rename_table :absence_schedules, :recurring_schedules
+    raise ActiveRecord::IrreversibleMigration
   end
 end
