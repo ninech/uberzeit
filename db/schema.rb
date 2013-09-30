6b9ddc97d6cc12aab48a75bc105fe84be51cffe0
Raffael Schmid <raf@nine.ch>
2013-09-30 10:06:45 +0200

    Add migration to generate all the timespans!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130926142856) do
+ActiveRecord::Schema.define(:version => 20130930075515) do
 
   create_table "absence_schedules", :force => true do |t|
     t.boolean  "active",                 :default => false
