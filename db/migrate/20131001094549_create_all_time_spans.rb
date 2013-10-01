76305e2847cb517c038a226e5c519d8ff765c279
Raffael Schmid <raf@nine.ch>
2013-10-01 15:33:44 +0200

    fix migration

--- db/migrate/20131001094549_create_all_time_spans.rb
+++ db/migrate/20131001094549_create_all_time_spans.rb
@@ -1,5 +1,6 @@
 class CreateAllTimeSpans < ActiveRecord::Migration
   def up
+    TimeSpan.reset_column_information
     say_with_time "Creating or updating TimeSpans of Absences" do
       Absence.all.each(&:update_or_create_time_span)
     end
