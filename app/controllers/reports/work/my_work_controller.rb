b5abec61fe12ec892de4361aafc3777e6b612ef6
Raffael Schmid <raf@nine.ch>
2013-10-04 12:54:26 +0200

    Remove before filter

--- app/controllers/reports/work/my_work_controller.rb
+++ app/controllers/reports/work/my_work_controller.rb
@@ -2,9 +2,9 @@ class Reports::Work::MyWorkController < ApplicationController
 
   load_and_authorize_resource :user
 
-  before_filter :set_year
-
   def year
+    @year = params[:year].to_i
+
     @range = UberZeit.year_as_range(@year)
     @table = Summarize::TableWithInterval.new(Summarize::Summarizer::Work, [@user], @range, 1.month)
 
@@ -19,16 +19,11 @@ class Reports::Work::MyWorkController < ApplicationController
 
   def month
     @month = params[:month].to_i
+    @year = params[:year].to_i
+
     @range = UberZeit.month_as_range(@year, @month)
     @users = [@user]
 
     @buckets = UberZeit.range_to_buckets(@range, 1.week, @range.min.beginning_of_week)
   end
-
-  private
-
-  def set_year
-    @year = params[:year].to_i
-  end
-
 end
