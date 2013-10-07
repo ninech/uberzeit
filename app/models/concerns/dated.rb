1636d2d216dc31854a5761809c33e1e6c23d5882
Nils Caspar <nils@nine.ch>
2013-10-07 17:11:30 +0200

    Fix naming

--- app/models/concerns/dated.rb
+++ app/models/concerns/dated.rb
@@ -3,7 +3,7 @@ module Dated
 
   module ClassMethods
     def scope_date(date_field_name)
-      [:add_date_between_scope, :add_date_not_between_scope, :add_year_month_scope, :add_year_scope].each do |method|
+      [:add_date_between_scope, :add_year_month_scope, :add_year_scope].each do |method|
         send(method, date_field_name)
       end
     end
@@ -11,7 +11,7 @@ module Dated
     private
     def add_date_between_scope(date_field_name)
       class_eval do
-        scope :"with_#{date_field_name}_between", ->(date_range) do
+        scope :"with_#{date_field_name}", ->(date_range) do
           if date_range.respond_to?(:min)
             where("#{date_field_name} BETWEEN ? AND ?", date_range.min, date_range.max)
           else
@@ -21,22 +21,10 @@ module Dated
       end
     end
 
-    def add_date_not_between_scope(date_field_name)
-      class_eval do
-        scope :"with_#{date_field_name}_not_between", ->(date_range) do
-          if date_range.respond_to?(:min)
-            where("NOT (#{date_field_name} BETWEEN ? AND ?)", date_range.min, date_range.max)
-          else
-            where("#{date_field_name} != ?", date_range)
-          end
-        end
-      end
-    end
-
     def add_year_month_scope(date_field_name)
       class_eval do
         scope :"with_#{date_field_name}_in_year_and_month", ->(year, month) do
-          send(:"with_#{date_field_name}_between", UberZeit.month_as_range(year, month))
+          send(:"with_#{date_field_name}", UberZeit.month_as_range(year, month))
         end
       end
     end
@@ -44,7 +32,7 @@ module Dated
     def add_year_scope(date_field_name)
       class_eval do
         scope :"with_#{date_field_name}_in_year", ->(year) do
-          send(:"with_#{date_field_name}_between", UberZeit.year_as_range(year))
+          send(:"with_#{date_field_name}", UberZeit.year_as_range(year))
         end
       end
     end
