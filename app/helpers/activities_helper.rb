2eb7cf1beadf6eac2269d1ab2fa858a24a396b54
Steven Schmid <steven@nine.ch>
2013-11-11 14:55:19 +0100

    show redmine and otrs when both are specified. fixes #8034

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,13 +1,15 @@
 module ActivitiesHelper
   def activity_source_information(activity)
-    [:otrs, :redmine].each do |source|
+    source_links = [:otrs, :redmine].collect do |source|
       ticket_id = activity.send("#{source}_ticket_id")
       if ticket_id
         url = link_to("##{ticket_id}", ticket_url(source, ticket_id))
-        return t(".#{source}", { ticket_id: ticket_id, ticket_url: url }).html_safe
+        t(".#{source}", { ticket_id: ticket_id, ticket_url: url }).html_safe
+      else
+        nil
       end
-    end
-    nil
+    end.compact
+    source_links.to_sentence.html_safe
   end
 
   def customer_link(customer_id)
