fe9aa1cb1c9a0a81cd74eee262faf6bf623f9087
Raffael Schmid <raf@nine.ch>
2013-11-12 18:10:22 +0100

    display all days, always

--- app/controllers/comparison_controller.rb
+++ app/controllers/comparison_controller.rb
@@ -12,9 +12,6 @@ class ComparisonController < ApplicationController
   def load_data_points_in_range
     @data_points = { time_entries: [], activities: [] }
     @range.each do |date|
-      if date > Date.today
-        break
-      end
       @data_points[:time_entries] << [(date.to_time.to_f * 1000.0).to_i, @user.time_sheet.effective_working_time_total(date).to_hours]
       @data_points[:activities] << [(date.to_time.to_f * 1000.0).to_i, (@user.activities.where(date: date).sum(:duration)/3600.0)]
     end
