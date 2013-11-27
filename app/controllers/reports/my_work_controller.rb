0031d1704f5334cc1ee3cdea3c63bbff27fb0c97
Raffael Schmid <raf@nine.ch>
2013-11-27 12:51:08 +0100

    fix user select of my work

--- app/controllers/reports/my_work_controller.rb
+++ app/controllers/reports/my_work_controller.rb
@@ -3,6 +3,11 @@ class Reports::MyWorkController < Reports::BaseController
   load_and_authorize_resource :user
 
   def show
+    if @year.nil?
+      redirect_to action: :show, year: Time.current.year, month: Time.current.month
+      return
+    end
+
     if @month
       @range = UberZeit.month_as_range(@year, @month)
       @buckets = UberZeit.range_to_buckets(@range, 1.week, @range.min.beginning_of_week)
