7eaab8e89360bdc494d37f0700f7e73cfe2ec637
Nils Caspar <nils@nine.ch>
2013-12-03 15:01:00 +0100

    Fix migration of customer number

--- db/migrate/20131118115555_add_customer_number.rb
+++ db/migrate/20131118115555_add_customer_number.rb
@@ -2,10 +2,24 @@ class AddCustomerNumber < ActiveRecord::Migration
   def up
     rename_column :customers, :id, :number
     add_column :customers, :id, :primary_key
+    fix_customer_references(Project, :number, :id)
+    fix_customer_references(Activity, :number, :id)
   end
 
   def down
+    fix_customer_references(Project, :id, :number)
+    fix_customer_references(Activity, :id, :number)
     remove_column :customers, :id
     rename_column :customers, :number, :id
+  end
+
+  private
+  def fix_customer_references(model, source_id_name, target_id_name)
+    model.all.each do |item|
+      next if item.customer_id.nil?
+      customer = Customer.where(source_id_name => item.customer_id).last
+      item.customer_id = customer.send(target_id_name)
+      item.save!(validate: false)
+    end
   end
 end
