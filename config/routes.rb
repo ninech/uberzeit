b7095b68f038d26be9fb307abf0266f462f1ea0b
Steven Schmid <steven@nine.ch>
2013-12-04 17:05:24 +0100

    fix filter fields

--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,7 @@ Uberzeit::Application.routes.draw do
     namespace :activities do
       get '/billability(/:date)', to: 'billability#index', as: :billability
       get '/billing', to: 'billing#index', as: :billing
-      get '/filter/:year/:month/:group_by', to: 'filter#index', as: :filter
+      get '/filter', to: 'filter#index', as: :filter
       get '/detailed', to: 'detailed#index', as: :detailed
     end
   end
