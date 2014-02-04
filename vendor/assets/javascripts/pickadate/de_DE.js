1a6ee439c6552deec4aa702f537199ca3f643f9d
Nils Caspar <nils@nine.ch>
2014-02-04 10:50:45 +0100

    Hack to only translate calendar when german locale selected, fixes #9844

--- vendor/assets/javascripts/pickadate/de_DE.js
+++ vendor/assets/javascripts/pickadate/de_DE.js
@@ -1,13 +1,17 @@
 // German
 
-$.extend( $.fn.pickadate.defaults, {
-    monthsFull: [ 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember' ],
-    monthsShort: [ 'Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez' ],
-    weekdaysFull: [ 'Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag' ],
-    weekdaysShort: [ 'So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa' ],
-    today: 'Heute',
-    clear: 'Löschen',
-    firstDay: 1,
-    format: 'dddd, dd. mmmm yyyy',
-    formatSubmit: 'yyyy/mm/dd'
+$(function() {
+  if($('html').attr('lang') == 'de') {
+    $.extend($.fn.pickadate.defaults, {
+      monthsFull: [ 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember' ],
+      monthsShort: [ 'Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez' ],
+      weekdaysFull: [ 'Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag' ],
+      weekdaysShort: [ 'So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa' ],
+      today: 'Heute',
+      clear: 'Löschen',
+      firstDay: 1,
+      format: 'dddd, dd. mmmm yyyy',
+      formatSubmit: 'yyyy/mm/dd'
+    });
+  }
 });
