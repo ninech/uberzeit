0be88fd302b7922abfce49f4218a6ca660c8fa54
Steven Schmid <steven@nine.ch>
2014-03-19 18:31:49 +0100

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,29 +8,8 @@
 
 if TimeType.count == 0
   TimeType.create(name: 'Work', is_work: true, is_vacation: false, color_index: 10, icon: :briefcase)
-  TimeType.create(name: 'On Duty', is_work: true, is_vacation: false, bonus_calculator: 'nine_on_duty', color_index: 11, icon: :road)
   TimeType.create(name: 'Vacation', is_work: false, is_vacation: true, color_index: 1, icon: :plane)
   TimeType.create(name: 'Illness', is_work: false, is_vacation: false, color_index: 2, icon: :medkit)
-  TimeType.create(name: 'Compensation', is_work: false, is_vacation: false, exclude_from_calculation: true, color_index: 7, icon: :'circle-blank')
-  TimeType.create(name: 'Absence by law', is_work: false, is_vacation: false, color_index: 8, icon: :legal)
-end
-
-if ActivityType.count == 0
-  ActivityType.create(name: 'Maintenance')
-  ActivityType.create(name: 'Setup Server')
-end
-
-if Customer.count == 0
-  Customer.create(id: 1, name: 'Yolo Inc')
-  Customer.create(id: 2, name: 'Nils\'s Vacuum Cleaners Inc')
-end
-
-if Project.count == 0
-  Project.create(customer_id: 1, name: 'Mustached Spice')
-  Project.create(customer_id: 1, name: 'Overwhelming Matterhorn')
-
-  Project.create(customer_id: 2, name: 'Cleaning with Fun Cluster 1')
-  Project.create(customer_id: 2, name: 'Demo @ nine HeadQuarters including an apero')
 end
 
 if User.count == 0
