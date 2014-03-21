6c06ffb04e85859831aef54a321ec4dbe245cbdf
Nils Caspar <ncaspar@me.com>
2014-03-21 10:53:28 +0100

    Check if airbrake is available

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -5,6 +5,6 @@ class ErrorsController < ApplicationController
   before_filter :notify_airbrake_if_needed
 
   def notify_airbrake_if_needed
-    notify_airbrake(@exception) if @status_code >= 500
+    notify_airbrake(@exception) if @status_code >= 500 && self.respond_to?(:notify_airbrake)
   end
 end
