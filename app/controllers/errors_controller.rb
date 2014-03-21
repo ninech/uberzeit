5193755f1766d3da7443570f88e5b9219a8c2bb3
Steven Schmid <stevschmid@gmail.com>
2014-03-21 17:23:55 +0100

    Make it beautiful

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,9 +2,9 @@ class ErrorsController < ApplicationController
   include Gaffe::Errors
   skip_before_filter :ensure_logged_in
 
-  before_filter :notify_airbrake_if_needed
+  before_filter :notify_airbrake_if_needed, if: { self.respond_to?(:notify_airbrake) }
 
   def notify_airbrake_if_needed
-    notify_airbrake(@exception) if @status_code >= 500 && self.respond_to?(:notify_airbrake)
+    notify_airbrake(@exception) if @status_code >= 500
   end
 end
