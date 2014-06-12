9c08f57392cac8f1de5e307325a7a555cb0049e3
Samuel Sieg <sasie@nine.ch>
2014-06-12 14:18:41 +0200

    Added some tests

--- spec/controllers/reports/overview_controller_spec.rb
+++ spec/controllers/reports/overview_controller_spec.rb
@@ -12,49 +12,114 @@ describe Reports::OverviewController do
   end
 
   context 'for signed-in users' do
-
     before do
-      test_sign_in user
-    end
-
-    describe 'GET "index"' do
-      it 'assigns the correct instance variables' do
-        get :index, user_id: user
-        assigns(:uberzeit).should_not be_nil
-        assigns(:month_total_work).should_not be_nil
-        assigns(:month_percent_done).should_not be_nil
-        assigns(:personal_absences).should_not be_nil
-        assigns(:team_absences).should_not be_nil
-        assigns(:vacation_redeemed).should_not be_nil
-        assigns(:vacation_remaining).should_not be_nil
-      end
-
-      it 'renders the :index template' do
-        get :index, user_id: user
-        response.should render_template :index
-      end
-
-      context 'with no planned work' do
-        before(:each) do
-          TimeSheet.any_instance.stub(:planned_working_time).and_return(0)
-        end
-
-        it 'does not raise an error' do
-          expect do
-            get :index, user_id: user
-          end.not_to raise_error
-        end
-      end
-
-      context 'on 1st january' do
-        before { Timecop.freeze('2014-01-01'.to_date) }
-
-        it 'does not raise an error' do
-          expect { get :index, user_id: user }.to_not raise_error
+      User.stub(:find) do |*args|
+        if args.first.to_i == user.id
+          user
+        else
+          User.find_by_id(args.first)
         end
       end
     end
 
+    shared_examples_for "assigned vars" do
+      it 'assigns overtime' do
+        user.time_sheet.stub(:overtime).and_return(10)
+        get :index, user_id: user
+        assigns(:uberzeit).should eq(10)
+      end
+
+      it 'assigns month_total_work' do
+        user.time_sheet.stub(:overtime).and_return(10)
+        user.time_sheet.stub(:planned_working_time).and_return(10)
+        get :index, user_id: user
+        assigns(:month_total_work).should eq(20)
+      end
+
+      it 'assigns month_percent_done' do
+        user.time_sheet.stub(:planned_working_time).and_return(20)
+        get :index, user_id: user
+        assigns(:month_percent_done).should eq(0)
+      end
+
+      describe 'absences' do
+        before do
+          FindDailyAbsences.stub(:new).and_return(OpenStruct.new(result_grouped_by_date: []))
+        end
+
+        it 'assigns personal_absences' do
+          FindDailyAbsences.should_receive(:new).with(user, Date.today..Date.today+7.days).and_return(OpenStruct.new(result_grouped_by_date: []))
+          get :index, user_id: user
+          assigns(:personal_absences).should eq([])
+        end
+
+        it 'assigns team_absences' do
+          FindDailyAbsences.should_receive(:new).with([], Date.today..Date.today+7.days).and_return(OpenStruct.new(result_grouped_by_date: []))
+          get :index, user_id: user
+          assigns(:team_absences).should eq([])
+        end
+      end
+
+      it 'assigns vacation_redeemed' do
+        user.time_sheet.stub(:redeemed_vacation).and_return(5)
+        get :index, user_id: user
+        assigns(:vacation_redeemed).should eq(5)
+      end
+
+      it 'assigns vacation_remaining' do
+        user.time_sheet.stub(:remaining_vacation).and_return(15)
+        get :index, user_id: user
+        assigns(:vacation_remaining).should eq(15)
+      end
+    end
+
+
+    context 'current user' do
+      before do
+        test_sign_in user
+      end
+
+      describe 'GET "index"' do
+        it_behaves_like "assigned vars"
+
+        it 'renders the :index template' do
+          get :index, user_id: user
+          response.should render_template :index
+        end
+
+        context 'with no planned work' do
+          before(:each) do
+            TimeSheet.any_instance.stub(:planned_working_time).and_return(0)
+          end
+
+          it 'does not raise an error' do
+            expect do
+              get :index, user_id: user
+            end.not_to raise_error
+          end
+        end
+
+        context 'on 1st january' do
+          before { Timecop.freeze('2014-01-01'.to_date) }
+
+          it 'does not raise an error' do
+            expect { get :index, user_id: user }.to_not raise_error
+          end
+        end
+      end
+    end
+
+
+    context 'other user' do
+      let(:admin) { FactoryGirl.create(:admin) }
+
+      before do
+        test_sign_in admin
+      end
+
+      describe 'GET "index"' do
+        it_behaves_like "assigned vars"
+      end
+    end
   end
-
 end
